50415a2e25722cd0f027be0e1fe034f3e84548c7
TheOneTrueLX <lx@opx.me>
2022-03-10 10:37:29 -0800

    Frontend setup for Tailwind CSS and socket.io

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import io from 'socket.io-client'
+import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$socketio = io('http://localhost:5000')
+
+app.mount('#app')
