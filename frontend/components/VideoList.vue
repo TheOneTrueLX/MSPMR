cb1a84274460c13f5628307691e37ad0f267e440
TheOneTrueLX <lx@opx.me>
2022-03-21 12:32:53 -0700

    scaffolding for vidoe list

---
+++ frontend/components/VideoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div><h2>On Deck:</h2></div>
+    <div class="bg-gray-600 mb-4 p-4">
+        Video goes here
+    </div>
+    <div><h2>Queue:</h2></div>
+    <div class="bg-gray-600 mb-4 p-4">
+        Video goes here
+    </div>
+    <div class="bg-gray-600 mb-4 p-4">
+        Video goes here
+    </div>
+    <div class="bg-gray-600 mb-4 p-4">
+        Video goes here
+    </div>
+  </div>
+</template>
+
+<style>
+  h2 {
+    font-family: 'Russo One', Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-size: 20pt;
+  }
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,57 @@
 <template>
-  <Tutorial />
+  <div class="m-6 p-4 bg-gray-800"> 
+    <div class="grid grid-cols-2">
+      <div class="justify-self-start">
+        <h1>MSPMR</h1>
+      </div>
+      <div class="justify-self-end items-center" v-if="user">
+        <div class="grid grid-cols-2 p-0 place-items-center">
+          <div><img class="rounded-full border-2" width="64" height="64" :src="user.profile_image_url" /></div>
+          <div>
+            <p class="font-bold">{{ user.display_name }}</p>
+            <p class="text-center">Logout</p>
+          </div>
+        </div>
+      </div>
+      <div class="justify-self-end items-center" v-else>
+        <NuxtLink to="/auth">Login</NuxtLink>
+      </div>
+    </div>
+    <div>
+      <VideoList></VideoList>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue';
-import { Socket } from 'socket.io-client'
-import { DefaultEventsMap } from '@socket.io/component-emitter'
 
 export default Vue.extend({
   name: 'IndexPage',
   data() {
     return {
-      socket: null as Socket<DefaultEventsMap, DefaultEventsMap> | null
+      user: null,
+      socket: null
     }
   },
-  mounted(): void {
+  mounted() {
     this.socket = this.$nuxtSocket({ channel: '/' })
+  },
+  async asyncData({ $axios }) {
+    let { data } = await $axios.get('/auth/user');
+    return { user: data.data[0] }
   }
 })
 </script>
+
+<style>
+  body {
+    background-color: #000000;
+    color: #ffffff;
+  }
+
+  h1 {
+    font-family: 'Russo One', Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-size: 40pt;
+  }
+</style>
