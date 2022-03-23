a7bb886e82266ec6e514fc6748813172fde01572
TheOneTrueLX <lx@opx.me>
2022-03-23 12:30:46 -0700

    moving video submission pre-processing into API

--- services/api/server/api/controllers/auth/controller.ts
+++ services/api/server/api/controllers/auth/controller.ts
@@ -1,9 +1,58 @@
 import redisClient from '../../../common/redis'
 import { Request, Response, NextFunction } from 'express';
-import axios from 'axios';
-import { promises as fs } from 'fs';
+import axios, { AxiosResponse } from 'axios';
+import fs from 'fs';
 import * as HttpStatus from 'http-status-codes';
 import L from '../../../common/logger'
+
+interface IAccessToken {
+  access_token: string,
+  refresh_token: string,
+  expires_in: number,
+  scope?: Array<string>,
+  token_type?: string
+}
+
+async function getAccessToken(): Promise<IAccessToken | null> {
+  try {
+    const stat = await fs.promises.stat('./tokens.json');
+
+    const accessToken: IAccessToken = JSON.parse(await fs.promises.readFile('./tokens.json', 'utf-8'));
+    if (stat.mtime.getTime() + (accessToken.expires_in * 1000) <= (new Date).getTime()) {
+      const payload = {
+        grant_type: 'refresh_token',
+        refresh_token: accessToken.refresh_token,
+        client_id: process.env.TWITCH_CLIENT_ID,
+        client_secret: process.env.TWITCH_CLIENT_SECRET      
+      };
+      const newAccessToken: AxiosResponse = await axios.post('https://id.twitch.tv/oauth2/token', payload);
+      redisClient.set('oauth_tokens', JSON.stringify(newAccessToken.data, null, 4));
+      await fs.promises.writeFile('./tokens.json', JSON.stringify(newAccessToken.data, null, 4), 'utf-8');
+      return newAccessToken.data as IAccessToken;
+    } else {
+      return accessToken;
+    }
+
+  } catch (err) {
+
+    if (err.code === "ENOENT") {
+      // tokens.json missing
+      L.warn('tokens.json not found - does user need to reauth?')
+      L.debug(JSON.stringify(err, null, 4))
+      return null
+    } else {
+      // something else went wrong but we're gonna do
+      // exactly the same thing
+      L.error(`Error reading token data: ${err}`)
+      L.debug(JSON.stringify(err, null, 4))
+    }
+
+  }
+  // The function shouldn't make it this far without
+  // returning something -- so we'll return null just
+  // as a failsafe.
+  return null;
+}
 
 export class Controller {
 
@@ -14,9 +63,8 @@ export class Controller {
         axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback`)
         .then((apiRes) => {
           L.info('Saving access token...');
-          // this.save(apiRes.data);
-          redisClient.set('oauth_tokens', JSON.stringify(req.body, null, 4));
-          fs.writeFile('./tokens.json', JSON.stringify(req.body, null, 4), 'utf-8');
+          redisClient.set('oauth_tokens', JSON.stringify(apiRes.data, null, 4));
+          fs.promises.writeFile('./tokens.json', JSON.stringify(apiRes.data, null, 4), 'utf-8');
           L.info(`saved Twitch API oauth2 tokens`);
         }).catch((err) => {
           L.error(err.toJSON());
@@ -32,24 +80,34 @@ export class Controller {
 
   async get(req: Request, res: Response, next: NextFunction) {
     try {
-      const accessToken = JSON.parse(await fs.readFile('./tokens.json', 'utf-8'));
-      L.debug(`${JSON.stringify(accessToken)}`)
-      // const authProvider = new StaticAuthProvider(process.env.TWITCH_CLIENT_ID, accessToken.access_token, accessToken.scope);
-      // const apiClient = new ApiClient({ authProvider });
-      // const user = apiClient.users.getMe();
-      axios.get('https://api.twitch.tv/helix/users', {
-          headers: {
-            'Authorization': `Bearer ${accessToken.access_token}`,
-            'Client-Id': process.env.TWITCH_CLIENT_ID
-          }
+      if(await redisClient.exists('user_info')) {
+        // user data cached so we get it from redis
+        L.info('Returning cached user data from redis')
+        const userInfo = JSON.parse(await redisClient.get('user_info'));
+        return res.status(HttpStatus.OK).json(userInfo);
+      } else {
+        // no user data cached so we get it from Twitch
+        const accessToken = await getAccessToken()
+        if (accessToken) {
+          axios.get('https://api.twitch.tv/helix/users', {
+              headers: {
+                'Authorization': `Bearer ${accessToken.access_token}`,
+                'Client-Id': process.env.TWITCH_CLIENT_ID
+              }
+            }
+          ).then((apires) => {
+            L.info('Successfully got broadcaster info from Twitch')
+            redisClient.set('user_info', JSON.stringify(apires.data, null, 4), { EX: 86400 })
+            return res.status(HttpStatus.OK).json(apires.data);
+          }).catch((err) => {
+            L.error(err)
+            L.debug(err.toJSON())
+            return next(err);
+          })     
+        } else {
+          return res.status(HttpStatus.UNAUTHORIZED).send()
         }
-      ).then((apires) => {
-        L.info('Successfully got broadcaster info from Twitch')
-        return res.status(HttpStatus.OK).json(apires.data);
-      }).catch((err) => {
-        L.error(err)
-        L.debug(err.toJSON())
-      })     
+      }
     } catch (err) {
       return next(err);
     }
@@ -58,7 +116,7 @@ export class Controller {
   async save(req: Request, res: Response, next: NextFunction) {
     try {
       await redisClient.set('oauth_tokens', JSON.stringify(req.body, null, 4));
-      await fs.writeFile('./tokens.json', JSON.stringify(req.body, null, 4), 'utf-8');
+      await fs.promises.writeFile('./tokens.json', JSON.stringify(req.body, null, 4), 'utf-8');
       L.info(`saved Twitch API oauth2 tokens`);
       return res.status(HttpStatus.NO_CONTENT).send();
     }
@@ -71,7 +129,7 @@ export class Controller {
     try {
       L.info(`deleting saved Twitch API auth tokens`);
       redisClient.del('oauth_tokens');
-      await fs.unlink('./tokens.json');
+      await fs.promises.unlink('./tokens.json');
       return res.status(HttpStatus.NO_CONTENT).send();
     }
     catch (err) {
--- services/api/server/api/controllers/videos/controller.ts
+++ services/api/server/api/controllers/videos/controller.ts
@@ -1,6 +1,15 @@
 import VideosService from '../../services/videos.service'
 import { Request, Response, NextFunction } from 'express';
+import { google } from 'googleapis'
 import * as HttpStatus from 'http-status-codes';
+import L from '../../../common/logger'
+import { IVideoModel } from '../../models/video';
+
+const videoServices = [
+  { youtube: /(.*\.)?youtu(\.be|be\.com)?/ },
+  // { twitch: /(.*\.)?twitch\.tv/ },
+  // { discord: /(.*\.)?discord\.gg/ }
+]
 
 export class Controller {
 
@@ -25,8 +34,89 @@ export class Controller {
   }
 
   async create(req: Request, res: Response, next: NextFunction) {
+    
+    const youtube = google.youtube({
+      version: 'v3',
+      auth: process.env.YT_API_KEY
+    })
+
+    // Get URL from channel point redeem message
+    var url: URL = null
+  
+    // Check to make sure the URL is a valid URL
     try {
-      const doc = await VideosService.create(req.body);
+      url = new URL(req.body.url);
+    } catch (err) {
+      // invalid/malformed URL
+      L.error(err);
+      return next(err);
+    }
+
+    // Check to make sure the URL points to a supported site
+    var service: string | boolean = false
+    videoServices.forEach((value) => {
+      // have to kind of jump through some hoops in order
+      // to test on regex inside of an array of objects
+      for(const property in value) {
+        if(value[property].test(url.hostname)) {
+          service = property
+        }
+      }
+    })
+
+    if(!service) {
+      // if we didn't match a service above, then
+      // we're assuming that the URL wasn't from
+      // a site that the app supports.
+      return res.status(HttpStatus.BAD_REQUEST).send();
+    }
+    
+    // pre-build payload for addition to the video queue
+    var video: IVideoModel = {
+      service_id: null,
+      service: service,
+      title: null,
+      duration: null,
+      copyrightClaimed: null,
+      redeemId: null,
+      submitter: req.body.submitter,
+      submissionDate: (new Date()).getTime(),
+      played: false
+    }
+
+    switch(service) {
+      case 'youtube':
+
+        try {
+          // fetch video information from YT API based on video ID
+          const videoInfo = await youtube.videos.list({
+            id: [url.searchParams.get('watch')],
+            part: ['snippet','contentDetails']
+          })
+    
+          // fill in the blanks on the payload
+          video.service_id = videoInfo.data.items[0].id;
+          video.title = videoInfo.data.items[0].snippet.description;
+          video.duration = (new Date(videoInfo.data.items[0].contentDetails.duration)).getTime();
+          video.copyrightClaimed = videoInfo.data.items[0].contentDetails.licensedContent;
+        } catch (err) {
+          // presumably a YT API error
+          L.error(err)
+          return next(err)         
+        }
+  
+        break;
+
+      case 'twitch':
+
+      case 'discord':
+
+      default:
+        return res.status(HttpStatus.NOT_IMPLEMENTED).send()
+    }
+
+    try {
+      const doc = await VideosService.create(video);
       return res.status(HttpStatus.CREATED).location(`/api/v1/videos/${doc._id}`).json(doc);
     }
     catch (err) {
