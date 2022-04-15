c8ba37fcac000190b1056dd4d10408f66591aa66
TheOneTrueLX <lx@opx.me>
2022-04-15 15:22:54 -0700

    improved error logging for user auth checking

--- services/api/server/api/middlewares/auth.handler.js
+++ services/api/server/api/middlewares/auth.handler.js
@@ -2,7 +2,7 @@ import l from '../../common/logger'
 
 export default function requireAuthorizedUser(req, res, next) {
     if(!req.session.user) {
-        l.warn(`Unauthorized access attempt to ${req.path}`)
+        l.warn(`Unauthorized access attempt to ${req.path}. This is expected if the user isn't authenticated, so don't panic unless you really weren't expecting to see this.`)
         res.status(401).json({ status: 401, message: 'Unauthorized'})
     }
     next();
