271e2e51ff25c0b9c6d93f08befca34eeea19098
TheOneTrueLX <lx@opx.me>
2022-04-21 08:23:30 -0700

    strip store to bare bones since only vue-router uses it now

--- frontend/src/store/actions.js
+++
@@ -1,4 +0,0 @@
-export const exampleAction = ({ commit }) => {
-    const data = 'foo'
-    commit('mutation', data)
-}
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,18 +1,3 @@
-import { createStore, createLogger } from 'vuex';
-import mutations from './mutations';
-import * as getters from './getters';
-import * as actions from './actions';
+import { createStore } from 'vuex';
 
-const state = {
-    username: null,
-    id: null,
-    profile_image: null,
-}
-
-export default createStore({
-    state,
-    getters,
-    actions,
-    mutations,
-    plugins: [createLogger()]
-})
+export default createStore({})
